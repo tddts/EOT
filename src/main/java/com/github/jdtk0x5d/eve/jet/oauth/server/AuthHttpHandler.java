4867eeae72de751d8ffaef3a69e277e3ecaef3bb
Tigran_Dadaiants <dtkcommon@gmail.com>
2017-03-12 22:12:19 +0400

    Fixed authorization response and request processing.

--- src/main/java/com/github/jdtk0x5d/eve/jet/oauth/server/AuthHttpHandler.java
+++ src/main/java/com/github/jdtk0x5d/eve/jet/oauth/server/AuthHttpHandler.java
@@ -39,6 +39,7 @@ public class AuthHttpHandler implements HttpHandler {
       URI requestURI = exchange.getRequestURI();
 
       logger.debug("Request URI:[" + requestURI + "]");
+      logger.debug("Query: " + requestURI.getQuery());
 
       if (exchange.getRequestMethod().equals(RequestMethod.GET.name())) {
 
@@ -47,23 +48,24 @@ public class AuthHttpHandler implements HttpHandler {
         if (requestURI.getQuery() == null) {
           logger.debug("Query is null.");
           response = responseText;
-          exchange.sendResponseHeaders(HttpStatus.OK.value(), response.length());
+          exchange.sendResponseHeaders(HttpStatus.OK.value(), 0);
         }
         else {
           logger.debug("Query is not null.");
           HttpStatus status = authService.processAuthorization(requestURI.getQuery());
           response = status.is2xxSuccessful() ? response : status.getReasonPhrase();
-          exchange.sendResponseHeaders(status.value(), response.length());
+          exchange.sendResponseHeaders(status.value(), 0);
         }
       }
 
     } finally {
-      logger.debug("Response: \n" + response);
 
-      OutputStream os = exchange.getResponseBody();
-      os.write(response.getBytes());
-      os.flush();
-      os.close();
+      logger.debug("Response: \n" + response + "");
+
+      try (OutputStream os = exchange.getResponseBody()) {
+        os.write(response.getBytes());
+        os.flush();
+      }
 
       logger.debug("Response stream closed.");
     }
