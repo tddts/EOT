0cc05d48c0a952c25d40398b80783513d87879ae
Tigran_Dadaiants <dtkcommon@gmail.com>
2017-06-18 03:07:46 +0400

    Added copyright notice.

--- src/main/java/com/github/jdtk0x5d/eve/jet/util/SpringUtil.java
+++ src/main/java/com/github/jdtk0x5d/eve/jet/util/SpringUtil.java
@@ -1,11 +1,25 @@
+/*
+ * Copyright 2017 Tigran Dadaiants
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.github.jdtk0x5d.eve.jet.util;
 
-import com.github.jdtk0x5d.eve.jet.context.Context;
 import org.apache.commons.lang3.tuple.Pair;
 import org.springframework.aop.framework.Advised;
 import org.springframework.aop.support.AopUtils;
 import org.springframework.beans.factory.BeanInitializationException;
-import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
 
 /**
  * @author Tigran_Dadaiants dtkcommon@gmail.com
@@ -25,5 +39,7 @@ public class SpringUtil {
       throw new BeanInitializationException(e.getMessage(), e);
     }
   }
+
+
 
 }
